cda65be2cbfd9df1a8edebd9094c3705abdb45e3
Daniel Stenberg <daniel@haxx.se>
2024-07-05 09:14:37 +0200

    mkpkg: build for 7.1 too, split into three separate ones
    One for each tarball

--- mkpkg-70.sh
+++ mkpkg-70.sh
@@ -3,72 +3,19 @@
 buildver=$1
 
 if [ -z "$buildver" ]; then
-  echo "Provide build version string"
+  echo "Provide build version string: x.y.z-n"
   exit
 fi
 
+tarball="`pwd`/_tarball"
 build="_build"
 install="$PWD/_install"
 
-# start a clean slate
-
-rm -rf $build
-
-###
-### 8.0 section
-###
-
-## 8.0 aarch64
-
-aprefix="$install/aarch64-8.0.0"
-atargetlibdir="$build/target/qnx/aarch64le/usr/lib/wolfssl"
-atargetbindir="$build/target/qnx/aarch64le/usr/bin/wolfssl"
-atargetincdir="$build/target/qnx/aarch64le/usr/include/wolfssl/curl"
-astrip="aarch64-unknown-nto-qnx8.0.0-strip"
-
-mkdir -p $atargetlibdir
-cp -p $aprefix/lib/libcurl.a $aprefix/lib/libcurl.so* $atargetlibdir
-cp -p $aprefix/lib/libcurl.so.12 $atargetlibdir/libcurl.so.12.sym
-$astrip $atargetlibdir/libcurl.so.12 $atargetlibdir/libcurl.so
-
-mkdir -p $atargetbindir
-cp -p $aprefix/bin/curl $atargetbindir
-cp -p $aprefix/bin/curl $atargetbindir/curl.sym
-$astrip $atargetbindir/curl
-
-mkdir -p $atargetincdir
-cp -p $aprefix/include/curl/*.h $atargetincdir
-
-## 8.0 x86_64
-
-xprefix="$install/x86_64-8.0.0"
-xtargetlibdir="$build/target/qnx/x86_64/usr/lib/wolfssl"
-xtargetbindir="$build/target/qnx/x86_64/usr/bin/wolfssl"
-xtargetincdir="$build/target/qnx/x86_64/usr/include/wolfssl/curl"
-xstrip="x86_64-pc-nto-qnx8.0.0-strip"
-
-mkdir -p $xtargetlibdir
-cp -p $xprefix/lib/libcurl.a $xprefix/lib/libcurl.so* $xtargetlibdir
-cp -p $xprefix/lib/libcurl.so.12 $xtargetlibdir/libcurl.so.12.sym
-$xstrip $xtargetlibdir/libcurl.so.12 $xtargetlibdir/libcurl.so
-
-mkdir -p $xtargetbindir
-cp -p $xprefix/bin/curl $xtargetbindir
-cp -p $xprefix/bin/curl $xtargetbindir/curl.sym
-$xstrip $xtargetbindir/curl
-
-mkdir -p $xtargetincdir
-cp -p $xprefix/include/curl/*.h $xtargetincdir
-
-## 8.0 documentation
-
-cp README-QNX.md "$build/target/qnx/"
+mkdir -p $tarball
 
 ###
 ### 7.0 section
 ###
-
-install="$HOME/qnx7-install"
 
 ## 7.0 aarch64
 
@@ -137,11 +84,8 @@ cp -p $aprefix/include/curl/*.h $atargetincdir
 
 cp README-QNX.md "$build/target/qnx7/"
 
-## build the 8.0 package
-
-cd $build
-tar czf curl-$buildver-qnxsdp8.0.tar.gz target/qnx
-
 ## build the 7.0 package
 
+cd $build
 tar czf curl-$buildver-qnxsdp7.0.tar.gz target/qnx7
+mv *tar.gz $tarball
