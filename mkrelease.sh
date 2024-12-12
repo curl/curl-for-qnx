42330a4cb179cf2fb315e317879c31da45b22291
Daniel Stenberg <daniel@haxx.se>
2024-12-12 10:52:09 +0100

    mkrelease: improve error message

--- mkrelease.sh
+++ mkrelease.sh
@@ -39,7 +39,7 @@ for dl in curl-*; do
 done
 
 if test "$i" -gt 1; then
-    echo "multiple curl-* entries found, disambiguate please"
+    echo "pre-existing curl-* entries found, disambiguate please"
     exit
 fi
 
